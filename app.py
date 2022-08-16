2e62bd4888ce57f839f058c66c0624335ee92280
hxyue1 <hxyue1@gmail.com>
2022-08-16 09:17:07 -0700

    add demo button

--- app.py
+++ app.py
@@ -6,20 +6,27 @@ from PIL import Image
 
 from model import VAEModel
 
-model = VAEModel(z_dim=160, n_filter_base=64, seed=1, dtype='float64')
-
-x = tf.random.normal((128,32,32,3))
-
-y = model(x, input_type='cifar10')
-
-model.load_weights('checkpoints/cp.ckpt')
-
-image = Image.open('demo_image.png')
+def load_model():
+    
+    # Initialise model
+    model = VAEModel(z_dim=160, n_filter_base=64, seed=1, dtype='float64')
+    
+    # Forward pass
+    x = tf.random.normal((128,32,32,3))    
+    y = model(x, input_type='cifar10')
+    
+    # Load weights
+    model.load_weights('checkpoints/cp.ckpt')
+    
+    return model
 
 # show = st.image(image, use_column_width=True)
 st.title("CVAE")
-st.sidebar.title("Upload Image")
 
+model = load_model()
+
+## Image upload
+st.sidebar.title("Upload Image")
 #Disabling warning
 st.set_option('deprecation.showfileUploaderEncoding', False)
 #Choose your own image
@@ -30,12 +37,21 @@ if uploaded_file is not None:
     u_img = Image.open(uploaded_file)
     st.image(u_img, 'Uploaded Image', use_column_width=True)
 
-    # We preprocess the image to fit in algorithm.
-    # image = np.asarray(u_img)/255
 
     image_tensor = tf.expand_dims(tf.keras.preprocessing.image.img_to_array(u_img)[:, :, :3], axis=0)
     
     gen_image = model(image_tensor/255, input_type='cifar10')
-
     
+    st.image(tf.math.sigmoid(gen_image[0][0]).numpy(), 'Generated Image', use_column_width=True)
+
+## Demo file
+st.sidebar.title("Run demo")
+if st.sidebar.button('Go!'):
+    
+    image = Image.open('demo_image.png')
+    st.image(image, 'Demo Image', use_column_width=True)
+    # We preprocess the image to fit in algorithm.
+    # image = np.asarray(image)
+    image_tensor = tf.expand_dims(tf.keras.preprocessing.image.img_to_array(image)[:, :, :3], axis=0)
+    gen_image = model(image_tensor/255, input_type='cifar10')
     st.image(tf.math.sigmoid(gen_image[0][0]).numpy(), 'Generated Image', use_column_width=True)
